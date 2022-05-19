5de9c600a6ebbf7bdd55353be8824a70dea6754a
c-o-b-r-a <gbvtg.7686@gmail.com>
2022-05-19 17:54:21 +0300

    auth

--- src/module/auth/auth.module.ts
+++ src/module/auth/auth.module.ts
@@ -5,7 +5,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthHelper } from './auth.helper';
 import { AuthService } from './auth.service';
-import { RoleModule } from '../role/role.module';
+
 import { UserModule } from '../user/user.module';
 import { getConfig } from '../../common/util';
 import { EConfig } from '../../common/config.enum';
@@ -14,7 +14,6 @@ import { EConfig } from '../../common/config.enum';
   controllers: [AuthController],
   imports: [
     ConfigModule,
-    RoleModule,
     UserModule,
     JwtModule.register({
       secret: getConfig(EConfig.DOCTORO_JWT_ACCESS_SECRET_KEY),
