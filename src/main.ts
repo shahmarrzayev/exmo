b7e710c8676bddc019150a60285f6c4448934873
Shahmar Rzayev <gbvtg.7686@gmail.com>
2022-05-31 19:13:19 +0400

    config updated

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
+import { getConfig } from './common/util';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { EConfig } from './common/config.enum';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3001);
+  await app.listen(parseInt(getConfig(EConfig.PORT)) || 5000);
 }
 bootstrap();
