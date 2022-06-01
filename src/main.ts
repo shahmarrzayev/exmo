f3563657dfd2079d88810edf761306080ea1ea83
c-o-b-r-a <76439036+c-o-b-r-a@users.noreply.github.com>
2022-06-01 16:02:33 +0400

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { EConfig } from './common/config.enum';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(parseInt(getConfig(EConfig.PORT)) || 5000);
 }
 bootstrap();
