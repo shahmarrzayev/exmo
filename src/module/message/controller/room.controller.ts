5d9b4a87237581bf410d297717185974db3e01a7
shahmarrzayev <gbvtg.7686@gmail.com>
2022-06-18 18:27:03 +0400

    update\

--- src/module/message/controller/room.controller.ts
+++ src/module/message/controller/room.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Req } from '@nestjs/common';
+import { Controller, Get, Param, Post, Req } from '@nestjs/common';
 import { RoomService } from '../service/room.service';
 import { IRequest } from '../../auth/interfaces/request.interface';
 import { RoomDto } from '../dto/room.dto';
@@ -6,6 +6,11 @@ import { RoomDto } from '../dto/room.dto';
 @Controller('/api/room')
 export class RoomController {
   constructor(private readonly roomService: RoomService) {}
+
+  @Post('/')
+  async create(@Req() req: IRequest): Promise<RoomDto[]> {
+    return;
+  }
 
   @Get('/')
   async getAll(@Req() req: IRequest): Promise<RoomDto[]> {
